2979ff6a87dc1e408664ebdd92d1201920988809
salman <salmanyaqoobpro@gmail.com>
2025-04-27 14:54:23 +0500

    card features completed now doing with product api with dummy api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
+import { useEffect, useState } from "react";
 import Cards from "./components/Cards";
+import { shoes } from "./contexts/shoesData.js";
+
+const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+// photos/random?count=5 █
 
 function App() {
+  const [imgSrc, setImgSrc] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchingImg() {
+      try {
+        const res = await fetch(
+          `https://api.unsplash.com/photos/random?query=shoes&count=10&client_id=${ACCESS_KEY}`,
+        );
+        const data = await res.json();
+        console.log(data);
+        setImgSrc(data);
+        setIsLoading(!setIsLoading);
+      } catch (err) {
+        console.error("something goes wrong when fetching photos" + err);
+      } finally {
+        console.log(imgSrc);
+      }
+    }
+
+    fetchingImg();
+  }, []);
+
   return (
     <div className="flex h-screen w-full flex-wrap items-center justify-center gap-1 bg-gray-200">
-      <Cards />
+      <img src="https://picsum.photos/200/300" alt="" />
+      {shoes.map((data, id) => (
+        <Cards
+          id={id}
+          key={data.id}
+          data={data}
+          isLoading={isLoading}
+          imgSrc={imgSrc}
+        />
+      ))}
     </div>
   );
 }
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,19 +1,29 @@
 import { FaCartShopping } from "react-icons/fa6";
 
-function Cards() {
+function Cards({ data, isLoading, imgSrc, id }) {
   return (
-    <div className="flex h-[450px] w-[300px] flex-col gap-1 overflow-hidden rounded-2xl border-2 border-slate-900 bg-slate-50 shadow-2xl shadow-gray-600">
-      <div className="w-full flex-1/3 overflow-hidden">
-        <img
-          src="/shoesBlack.jpg"
-          alt="Balck Shoes"
-          className="h-full w-full"
-        />
-      </div>
-      <div className="flex flex-col gap-2 px-5 py-1">
+    <div className="flex h-[400px] w-[270px] flex-col gap-1 overflow-hidden rounded-2xl border-2 border-slate-900 bg-slate-50 shadow-2xl shadow-gray-600">
+      {/* <div className="relative"> */}
+      {isLoading && (
+        <div className="flex h-full w-full items-center justify-center bg-gray-400">
+          <p>Error</p>
+        </div>
+      )}
+
+      {imgSrc && (
+        <div className="flex h-full w-full overflow-hidden">
+          <img
+            src={imgSrc[id].urls.small}
+            alt="Balck Shoes"
+            className="h-full w-full object-fill"
+          />
+        </div>
+      )}
+
+      <div className="flex flex-2/3 flex-col gap-3 px-5 py-1">
         <div className="flex flex-col">
-          <h4 className="text-xs text-gray-700">shoes</h4>
-          <h1 className="text-lg font-bold text-gray-950">Casual Shoes</h1>
+          <h4 className="text-xs text-gray-700">{data.category}</h4>
+          <h1 className="text-lg font-bold text-gray-950">{data.name}</h1>
           <p>
             {Array.apply(null, { length: 5 }).map(() => (
               <span key={Math.random()}>⭐</span>
@@ -23,12 +33,23 @@ function Cards() {
         </div>
         <div className="flex items-center justify-between text-blue-700">
           <p className="text-center text-lg">
-            $80 <span className="text-sm text-gray-500 line-through">$120</span>
+            ${data.price}{" "}
+            <span className="text-sm text-gray-500 line-through">
+              ${data.oldPrice}
+            </span>
           </p>
-          <button className="flex items-center justify-center rounded-sm border-2 border-gray-600 bg-blue-300 px-5 transition-colors duration-200 hover:bg-blue-200">
+          <button className="flex items-center justify-center gap-1 rounded-sm border-2 border-gray-600 bg-blue-300 px-5 transition-colors duration-200 hover:bg-blue-200">
             <FaCartShopping />
             add
           </button>
+        </div>
+        <div className="flex flex-col">
+          <p className="text-xs text-gray-400">Description :</p>
+          <p className="text-sm text-gray-800">
+            {data.description.length > 60
+              ? data.description.substring(0, 60) + "..."
+              : data.description}
+          </p>
         </div>
       </div>
     </div>
